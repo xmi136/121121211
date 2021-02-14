f397fe656b43374bba6841267d4b12b232af6284
xmi136 <78466586+xmi136@users.noreply.github.com>
2021-02-14 22:24:05 +0800

    Update diy-1.sh

--- build/openwrt/diy-1.sh
+++ build/openwrt/diy-1.sh
@@ -9,5 +9,7 @@
 #
 # 请不要直接套用我其他机型的扩展或者插件，每个源码有些文件路径都多多少少不一样，直接套用会出错
 #
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+git clone https://github.com/281677160/openwrt-package package/123
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
