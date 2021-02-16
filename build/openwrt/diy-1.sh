dd0336e63df97745439290572cb1547a47b305e6
xmi136 <78466586+xmi136@users.noreply.github.com>
2021-02-16 16:07:49 +0800

    Update diy-1.sh

--- build/openwrt/diy-1.sh
+++ build/openwrt/diy-1.sh
@@ -11,5 +11,7 @@
 #
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-git clone https://github.com/281677160/openwrt-package package/123
+#git clone https://github.com/281677160/openwrt-package package/123
 #git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone https://github.com/281677160/openwrt-package/tree/master/luci-app-adguardhome package/1
+git clone https://github.com/281677160/openwrt-package/tree/master/luci-app-smartdns package/2
